e301ae9f227a7e4a9ac7709bc702c477b0e53c67
Desislava Valova <denkova.de@gmail.com>
2017-02-04 23:22:19 +0200

    problem with changes on unittest

--- tests/test_helloPV.py
+++ tests/test_helloPV.py
@@ -3,10 +3,10 @@ import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-import hello as h
+import helloPV as h
 
 
 def test_helloPV():
-	a = h.hello()
+	a = h.helloPV()
 
 	assert a == "Hello, PV!"
