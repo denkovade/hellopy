c7507ab2711281fa52dfad98e82f309c76339b85
Desislava Valova <denkova.de@gmail.com>
2017-02-04 22:42:35 +0200

    problem with unittest

---
+++ helloPV.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+
+def hello():
+	return "Hello, PV!"
+
+
+def main():
+	print hello()
+
+
+if __name__ == "__main__":
+	main()
--- tests/test_helloPV.py
+++ tests/test_helloPV.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import hello as h
 
 
-def test_hello():
+def test_helloPV():
 	a = h.hello()
 
 	assert a == "Hello, PV!"
